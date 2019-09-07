d482be0238ece16050942455210bf6750804375f
olesgedz <olesgedz@rambler.ru>
2019-09-07 17:09:14 +0300

    sd

---
+++ srcs/ft_vec3_angle.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_vec3_angle.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jblack-b <jblack-b@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/09/07 16:50:55 by jblack-b          #+#    #+#             */
+/*   Updated: 2019/09/07 17:08:52 by jblack-b         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libmath.h"
+
+double	ft_vec3_angle(t_vec3 a, t_vec3 b)
+{
+	t_vec3	v1;
+	t_vec3	v2;
+
+	v1 = ft_vec3_normalize(a);
+	v2 = ft_vec3_normalize(b);
+	return (acos(ft_vec3_dot_multiply(v1, v2)));
+}
